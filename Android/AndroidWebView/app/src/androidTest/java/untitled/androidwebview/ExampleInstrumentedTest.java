b9e3d3ae13e3aea1d213c53d0d9edaea87b8b522
jpatena <patenaj@coyote.csusb.edu>
2022-03-03 12:56:14 -0800

    Got it working on Android Studio Bumblebee

--- Android/AndroidWebView/app/src/androidTest/java/untitled/androidwebview/ExampleInstrumentedTest.java
+++ Android/AndroidWebView/app/src/androidTest/java/untitled/androidwebview/ExampleInstrumentedTest.java
@@ -1,10 +1,20 @@
 package untitled.androidwebview;
 
-import android.content.Context;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.content.Context;
+import android.view.View;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matcher;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,12 +26,18 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("untitled.androidwebview", appContext.getPackageName());
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void testLaunch() {
+        // Context of the app under test.
+        //Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        onView(withText("AndroidWebView")).check(matches(isDisplayed()));
+        //assertEquals("untitled.androidwebview", appContext.getPackageName());
     }
 }
