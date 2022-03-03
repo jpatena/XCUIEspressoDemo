387c1e125dbc4dafb0eb87d4d9a1a8d020e5e3fe
jpatena <patenaj@coyote.csusb.edu>
2022-03-02 16:16:04 -0800

    Added Android webview

---
+++ Android/AndroidWebView/app/src/main/java/untitled/androidwebview/MainActivity.java
@@ -0,0 +1,25 @@
+package untitled.androidwebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.saucedemo.com");
+
+    }
+}
